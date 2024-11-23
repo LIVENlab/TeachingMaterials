4b416328aac01d17ffec8ffbee4b90e821f5c95b
CML <cristina.madrid@uab.cat>
2024-11-24 00:48:07 +0100

    Structured files

---
+++ 1_SetUp.py
@@ -0,0 +1,21 @@
+# Introduction to Brightway2
+# This material is prepared for the course on Green Engineering, part of the RICSE master program at UAB.
+
+# https://guies.uab.cat/guies_docents/public/portal/html/2024/assignatura/44729/es
+# https://www.uab.cat/web/estudiar/official-master-s-degrees/general-information-1096480962610.html?param1=1345875382068
+
+#  It is prepared by Cristina Madrid (cristina.madrid@uab.cat) using material from the Brightway2 documentation and the Brightway Book. MOre info:
+#  https://learn.brightway.dev/en/latest/content/chapters/BW25/BW25_introduction.html
+
+#  We will be working with BW 2.5 (Note that the BW version you installed for Activity Browser is not the same. You need to install the correct version).
+
+# As indicated in the BW2 installation site, you first need to install a distribution of python. Be sure to install python 3.11. More info:
+# https://docs.brightway.dev/en/latest/content/installation/index.html
+# https://www.python.org/downloads/).
+
+# After you have python installed, we need to create a dedicated python environment and install Brightway 2.5 and other necessary packages.
+# You can find all the packages needed in the requirements.txt file.
+
+pip install -r requirements.txt
+
+
